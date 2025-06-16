05671ca21d5edeb5c0502f44adb42e7141089e99
matanya alkobi <matanor123456@gmail.com>
2025-06-16 09:51:22 +0300

    יצירת כל הישויות  לשלב 1

---
+++ Entities/Pepoles/Pepole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGame.Entities.Pepoles
+{
+    internal abstract class Pepole
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Type { get; set; }
+
+        public Pepole(int ID, string FirstName, string LastName, string Type)
+        {
+            this.ID = ID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Type = Type;
+        }
+
+    }
+}
