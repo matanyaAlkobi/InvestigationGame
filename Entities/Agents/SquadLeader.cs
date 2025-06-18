d5fae545e0aa9443deb4809e455caa55d73c53e0
matanya alkobi <matanor123456@gmail.com>
2025-06-18 15:11:06 +0300

    In the SquadLeader class, adding the ability to attack back every 3 turns, Split the RemovesAAttachSensor method into a solid principle, Added documentation in the squad leader class, Arranged a game class to be run with the new agent, Arranged a game class to be solid.

--- Entities/Agents/SquadLeader.cs
+++ Entities/Agents/SquadLeader.cs
@@ -8,13 +8,82 @@ using InvestigationGame.Entities.Sensors;
 
 namespace InvestigationGame.Entities.Agents
 {
+
+    //  A class that handles a squadleader agent
     internal class SquadLeader : FootSoldier
     {
-        public SquadLeader(string rank = "SquadLeader") : base (rank)
+        int TurnCounter = 0;
+        static Random rnd = new Random();
+
+
+        /// <summary>
+        /// Constructs a soldier with a default value
+        /// </summary>
+        /// <param name="rank">defult to SquadLeader</param>
+        public SquadLeader(string rank = "SquadLeader") : base(rank)
         {
             this.Rank = rank;
             SensorWeakSpot = ConnectingSensors.SensorGenerator(this);   // Adding sensors to the list
             SensorDamage = new Sensor[SensorWeakSpot.Count];
         }
+
+
+
+        /// <summary>
+        /// Handles the logic for retaliatory sensor removal by the Iranian agent every 3 turns.
+        /// If any sensors are active, one is randomly removed and a message is displayed.
+        /// </summary>
+        public void RemovesAAttachSensor()
+        {
+            ++TurnCounter;
+            if (TurnCounter % 3 == 0)
+            {
+                if (!HasActiveSensors()) return;
+                RemoveRandomSensor();
+                PrintSensorRemovedMessage();
+            }
+        }
+
+        /// <summary>
+        /// Checks for any active sensors.
+        /// </summary>
+        /// <returns>True if a sensor should be removed; otherwise, false.</returns>
+        private bool HasActiveSensors()
+        {
+            if (!SensorDamage.Any(s => s != null))
+            {
+                Console.WriteLine("No active sensors to remove.");
+                return false;
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// Randomly selects and removes an active sensor from the agent.
+        /// </summary>
+        private void RemoveRandomSensor()
+        {
+            while (true)
+            {
+                int random = rnd.Next(0, SensorDamage.Length);
+                if (SensorDamage[random] != null)
+                {
+                    SensorDamage[random] = null;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prints a message to the console indicating that a sensor was removed.
+        /// </summary>
+        private void PrintSensorRemovedMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("The Iranian agent retaliated and removed a sensor.");
+            Console.ResetColor();
+        }
+
     }
 }
--- SystemOperation/Game.cs
+++ SystemOperation/Game.cs
@@ -10,31 +10,72 @@ using InvestigationGame.Entities.Agents;
 using InvestigationGame.Entities.Pepoles;
 using InvestigationGame.Entities.Sensors;
 using InvestigationGame.SetForGame;
+using InvestigationGame.SystemOperation.GUI;
 
 namespace InvestigationGame.SystemOperation
 {
     //A department responsible for the game's operating system
     internal class Game
     {
-        // Starting the game
-        public static void Start()
+        static bool exposed;
+
+        /// <summary>
+        /// Starts the game with a basic FootSoldier agent and progresses upon success.
+        /// </summary>
+        //public  void Start()
+        //{
+        //    List<Sensor> SensorsFactory = new List<Sensor> { new AudioSensor(), new ThermalSensor() };
+        //    FootSoldier agent = new FootSoldier();
+        //    //SquadLeader LeaderAgent = new SquadLeader();
+
+        //    Console.WriteLine("Welcome  Interrogator !!!");
+        //    do
+        //    {
+        //        Console.WriteLine($"The agent has {agent.SensorWeakSpot.Count} sensors");
+        //        int index =  GameInputManager.GetValidSensorIndexFromUser(agent);
+        //        string SensorName = GameInputManager.GetValidSensorNameFromUser(agent);
+        //        exposed = AgentExposureAnalyzer.TryExposeAgentWithSensor(SensorName, index, agent);
+        //    }
+        //    while (!exposed);
+        //    Console.ForegroundColor = ConsoleColor.Blue;
+        //    Console.WriteLine($"Well done, you managed to break an {agent.Rank} Irain.");
+        //    Console.WriteLine("You will advance in rank to a more difficult agent.");
+        //    Console.ResetColor();
+        //    Level2(LeaderAgent);
+        //}
+
+
+        /// <summary>
+        /// Plays a turn with the given agent and handles agent-specific behavior.
+        /// </summary>
+        /// <param name="agent">The agent to play with.</param>
+        public void Play(FootSoldier agent)
         {
-            //List<Sensor> SensorsFactory = new List<Sensor> { new AudioSensor(), new ThermalSensor() };
-            FootSoldier agent = new FootSoldier();
-            SquadLeader LeaderAgent = new SquadLeader();
-            Console.WriteLine("Welcome  Interrogator !!!");
-            bool exposed;
+            AgentConsoleView.ShowWelcomeMessage(agent);
             do
             {
                 Console.WriteLine($"The agent has {agent.SensorWeakSpot.Count} sensors");
-                int index =  GameInputManager.GetValidSensorIndexFromUser(agent);
+                int index = GameInputManager.GetValidSensorIndexFromUser(agent);
                 string SensorName = GameInputManager.GetValidSensorNameFromUser(agent);
                 exposed = AgentExposureAnalyzer.TryExposeAgentWithSensor(SensorName, index, agent);
+                HandleAgentBehavior(agent);
             }
             while (!exposed);
+            AgentConsoleView.DisplaySuccessMessage(agent);
         }
 
+        /// <summary>
+        /// Handles behavior specific to the agent's role.
+        /// </summary>
+        /// <param name="agent">The agent to evaluate.</param>
+        private void HandleAgentBehavior(FootSoldier agent)
+        {
+            if (agent is SquadLeader squad)
+            {
+                squad.RemovesAAttachSensor();
+            }
 
+        }
 
     }
 }
